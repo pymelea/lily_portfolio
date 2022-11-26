3681b3ddeba779086e2d77b5b5eedf1326227feb
Lily Perera <lilycapetillo86@gmail.com>
2022-11-25 21:56:29 -0300

    Changes in admin.py , adding models to admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+# Home
+admin.site.register(Home)
+
+# About
+class ProfileInline(admin.TabularInline):
+    model = Profile
+    extra = 1
+
+
+@admin.register(About)
+class AboutAdmin(admin.ModelAdmin):
+    '''
+        Admin View for About
+    '''
+    inlines = [
+        ProfileInline,
+    ]
+
+
+class SkillInline(admin.TabularInline):
+    '''
+        Admin View for Skills
+    '''
+    model = Skill
+    extra = 2
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    '''
+        Admin View for
+    '''
+    inlines = [
+        SkillInline,
+    ]
+
+admin.site.register(Portfolio)
+admin.site.register(Certificate)
+admin.site.register(Project)
+admin.site.register(Language)
+admin.site.register(Education)
