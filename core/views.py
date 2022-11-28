a784d15a14f29fea3041ed75e15f1f317b3c11ab
Lily Perera <lilycapetillo86@gmail.com>
2022-11-28 18:58:41 -0300

    Add the views to core app

--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'core/index.html')
+
+
+def about(request):
+    return render(request, 'core/about.html')
+
+
+def skill(request):
+    return render(request, 'core/skill.html')
+
+
+def portfolio(request):
+    return render(request, 'core/portfolio.html')
+
+
+def contact(request):
+    return render(request, 'core/contact.html')
