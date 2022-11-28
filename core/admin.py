8102a5b585e282d687924b6e405dba57e7f48a3e
Lily Perera <lilycapetillo86@gmail.com>
2022-11-28 18:54:02 -0300

    Refactoring admin model

--- core/admin.py
+++ core/admin.py
@@ -1,42 +1,50 @@
 from django.contrib import admin
 from .models import *
 
+
 # Register your models here.
-# Home
+
+
 admin.site.register(Home)
+admin.site.register(Portfolio)
+admin.site.register(Certificate)
+admin.site.register(Project)
+admin.site.register(Language)
+admin.site.register(Education)
+
 
 # About
 class ProfileInline(admin.TabularInline):
     model = Profile
     extra = 1
 
+
 @admin.register(About)
 class AboutAdmin(admin.ModelAdmin):
-    '''
-        Admin View for About
-    '''
+    """
+    Admin View for About
+    """
     inlines = [
         ProfileInline,
     ]
 
+
 class SkillInline(admin.TabularInline):
-    '''
-        Admin View for Skills
-    '''
+    """
+    Admin View for Skills
+    """
     model = Skill
     extra = 2
 
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    '''
-        Admin View for
-    '''
+    """
+    Admin View for Category
+    """
     inlines = [
         SkillInline,
     ]
 
-admin.site.register(Portfolio)
-admin.site.register(Certificate)
-admin.site.register(Project)
-admin.site.register(Language)
-admin.site.register(Education)
+
+
