437fe3342bd5a40a91ac5337219185dcb1a6ef81
Lily Perera <lilycapetillo86@gmail.com>
2022-11-30 14:06:05 -0300

    Creating access the data of home, about, profiles, categories,portfolios since index view

--- core/views.py
+++ core/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 
+from core.models import Home, About, Profile, Category, Portfolio
+
+
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'core/index.html')
+    # Home
+    home = Home.objects.latest('updated')
+
+    # About
+    about = About.objects.latest('updated')
+
+    # Profile
+    profiles = Profile.objects.filter(about=about)
+
+    # Skill
+    categories = Category.objects.all()
+
+    # Portfolio
+    portfolios = Portfolio.objects.all()
+    context = {
+        'home': home,
+        'about': about,
+        'profiles': profiles,
+        'categories': categories,
+        'portfolios': portfolios,
+    }
+    return render(request, 'core/index.html', context)
 
 
 def about(request):
