f9752511a1d8bd466af2fdff66dbd7753e728efe
clicly <lilycapetillo86@gmail.com>
2022-12-21 23:50:34 -0300

    Changed in build file to dont up staticfiles

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
 echo "Collect Static..."
-#python3 manage.py collectstatic --noinput --clear
+# python3 manage.py collectstatic --noinput --clear
