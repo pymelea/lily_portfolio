4700aeeace911e3be19860fadc7792c965f8d405
clicly <lilycapetillo86@gmail.com>
2022-12-22 00:08:13 -0300

    |Changed staticfile configurations in settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
 STATIC_ROOT = BASE_DIR / "staticfiles_build" /  "static"
 
 STATICFILES_DIRS = [
