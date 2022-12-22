2d17951b148f86dd49b1d5e00b70f09c8d6d28fa
clicly <lilycapetillo86@gmail.com>
2022-12-21 23:06:54 -0300

    Changed build.sh file

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ echo "Building the project..."
 python3 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
+# python3 manage.py makemigrations --noinput
+# python3 manage.py migrate --noinput
 
 echo "Collect Static..."
 python3 manage.py collectstatic --noinput --clear
