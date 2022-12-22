10a9fd5782f4a4687dbdbf9a4251671979e84659
clicly <lilycapetillo86@gmail.com>
2022-12-21 22:55:05 -0300

    Changed in build the used python

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 # Build the project
 echo "Building the project..."
-python3.10 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.10 manage.py makemigrations --noinput
-python3.10 manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.10 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
