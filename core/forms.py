e8e2ccbe6c0fda002e8a5c9f100e5653f61dd4b6
clicly <lilycapetillo86@gmail.com>
2022-12-26 15:12:59 -0300

    Editing ConatctForm

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    from_email = forms.EmailField(required=True)
-    subject = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    email = forms.EmailField(required=True)
+    name = forms.CharField(required=True)
+    subject = forms.CharField(widget=forms.Textarea, required=True)
 
