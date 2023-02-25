ed5558acc6157fd5d0f029fb2c2d6f64977cf828
Lily Perera <lilycapetillo86@gmail.com>
2023-02-25 16:26:51 -0300

    Try install python 3.10

--- build.sh
+++ build.sh
@@ -2,10 +2,18 @@
 
 # Install python 3.10
 yum update -y
+yum groupinstall "Development Tools" -y
+yum erase openssl-devel -y
+yum install openssl11 openssl11-devel  libffi-devel bzip2-devel wget -y
+wget https://www.python.org/ftp/python/3.10.4/Python-3.10.4.tgz
+tar -xf Python-3.10.4.tgz
+(cd Python-3.10.4 && ./configure --enable-optimizations && make altinstall) 
+yum install python3-pip
+python3.10 -m pip install -r requirements.txt
 
 # Build the project
 echo "Building the project..."
-pip install -r requirements.txt
+# pip install -r requirements.txt
 
 cat /etc/os-release
 
