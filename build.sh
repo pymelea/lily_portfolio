9ee5cc75b81dcf4b9a8aaabe4aebce496bcad872
Lily Perera <lilycapetillo86@gmail.com>
2023-02-25 17:10:05 -0300

    Fixed #10 Downgrade DJango to 3.2.18

--- build.sh
+++ build.sh
@@ -2,19 +2,19 @@
 
 # Install sqlite 3.8
 
-yum update -y
-yum groupinstall "Development Tools" -y
-yum erase openssl-devel -y
-yum erase sqlite -y
-yum install openssl11 openssl11-devel  libffi-devel bzip2-devel wget -y
-wget https://www.sqlite.org/2023/sqlite-autoconf-3410000.tar.gz
-tar zxvf sqlite-autoconf-3410000.tar.gz
-(cd sqlite-autoconf-3410000 && ./configure && make && make install)
-rm -rfv sqlite-autoconf-3410000
+# yum update -y
+# yum groupinstall "Development Tools" -y
+# yum erase openssl-devel -y
+# yum erase sqlite -y
+# yum install openssl11 openssl11-devel  libffi-devel bzip2-devel wget -y
+# wget https://www.sqlite.org/2023/sqlite-autoconf-3410000.tar.gz
+# tar zxvf sqlite-autoconf-3410000.tar.gz
+# (cd sqlite-autoconf-3410000 && ./configure && make && make install)
+# rm -rfv sqlite-autoconf-3410000
 
-# Build the project
-echo "Building the project..."
-pip install -r requirements.txt
+# # Build the project
+# echo "Building the project..."
+# pip install -r requirements.txt
 
 cat /etc/os-release
 
