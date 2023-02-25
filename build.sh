edb1a78720bbd285aeba5dc33b1f4f136ea1ca62
Lily Perera <lilycapetillo86@gmail.com>
2023-02-25 14:23:44 -0300

    Fixed #10 Comments all jobs in build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@
 echo "Building the project..."
 pip install -r requirements.txt
 
-echo "Make Migration..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+# echo "Make Migration..."
+# python manage.py makemigrations --noinput
+# python manage.py migrate --noinput
 
-echo "Collect Static..."
-python manage.py collectstatic --noinput --clear
+# echo "Collect Static..."
+# python manage.py collectstatic --noinput --clear
