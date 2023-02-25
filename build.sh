52abd9413713e6c86555f3b793851f38c8730926
Lily Perera <lilycapetillo86@gmail.com>
2023-02-25 16:50:36 -0300

    Remove all dependecy

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Install python 3.10
+# Install sqlite 3.8
 yum update -y
 yum groupinstall "Development Tools" -y
 yum erase openssl-devel -y
 yum install openssl11 openssl11-devel  libffi-devel bzip2-devel wget -y
 wget https://www.sqlite.org/2023/sqlite-autoconf-3410000.tar.gz
 tar zxvf sqlite-autoconf-3410000.tar.gz
-(cd sqlite-autoconf-3410000 && ./configure && make && make install) 
-yum install python3-pip
+(cd sqlite-autoconf-3410000 && ./configure && make && make install)
+rm -rfv sqlite-autoconf-3410000
 
 # Build the project
 echo "Building the project..."
